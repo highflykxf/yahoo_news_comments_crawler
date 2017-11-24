179ff25901ee5ee736280f4910db16b32fd2e561
highfly <xiangfei0919@gmail.com>
2017-11-24 19:11:19 +0800

    enhance the parse_comemnt function

--- crawler.py
+++ crawler.py
@@ -7,6 +7,9 @@ from bs4 import BeautifulSoup
 # from goose import Goose
 from selenium import webdriver
 import MySQLdb
+import time
+import datetime
+
 
 mainpage_url = 'http://news.yahoo.com/us/most-popular/'
 
@@ -73,6 +76,7 @@ def parse_news_from_mainpage(mainpage_content):
 
 
 def parse_news_title_and_content(news_url):
+    print news_url
     # 设置chrome选项：不加载图片
     chromeOptions = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_settings.images": 2}
@@ -80,26 +84,31 @@ def parse_news_title_and_content(news_url):
     driver = webdriver.Chrome(chrome_options=chromeOptions)
     driver.set_script_timeout(30)
     driver.get(news_url)
+    time.sleep(20)
     webpage_content = driver.page_source
     soup = BeautifulSoup(webpage_content, "html.parser")
 
     title = soup.find('h1', {'itemprop': 'headline'}).string
     news_time = soup.find('time', {'itemprop': 'datePublished'}).string
     l_p = soup.find_all('p', {'type': "text"})
-    c_num = re.findall("\d+", str(soup.find(text=re.compile(r'\d+\sreactions')).encode('utf-8')))[0]
+    c_num = 2
+    # if soup.find(text=re.compile(r'\d+\sreactions')) != None:
+    #     c_num = re.findall("\d+", str(soup.find(text=re.compile(r'\d+\sreactions')).encode('utf-8')))[0]
     press_name = soup.find('span', {'class': 'provider-link'}).string
     content_id = soup.find('article', {'itemprop': 'articleBody'})
+    if content_id is None:
+        return None
     c_id = content_id['data-uuid']
 
     l_content = []
     for p in l_p:
-        if p.content:
-            l_content.append(p['content'])
+        l_content.append(p['content'])
     content = ''
     content = '\n'.join(l_content).encode('utf-8')
 
     news_dict = {}
     news_dict['title'] = title.encode('utf-8')
+    news_dict['news_url'] = news_url
     news_dict['content'] = content
     news_dict['comment_num'] = int(c_num)
     news_dict['press_name'] = press_name.encode('utf-8')
@@ -112,39 +121,45 @@ def parse_news_title_and_content(news_url):
 def parse_comments(session, content_url, news_dict):
     print content_url
     sentiment_count = {}
-    canvassMessages = []
+
     try:
         head, content = httplib2.Http().request(content_url)
         json_data = json.loads(content)
         data = json_data['data']
-        readingUserCount = data['userActivityNotification']['readingUserCount']
+
+        rurl_new = urlencode(news_dict['content_id'], data['total']['count'])
+        head, content = httplib2.Http().request(rurl_new)
+        json_data = json.loads(content)
+        data = json_data['data']
+
+        readingUserCount = data['userActivityNotification']['readingUsersCount']
         sentiments = data['sentiments']
         for s_c in sentiments:
             sentiment_count[s_c['sentiment']] = s_c['count']
-
-        for c_id, comment in enumerate(data['canvasMessages']):
+        canvassMessages = [''] * len(data['canvassMessages'])
+        for c_id, comment in enumerate(data['canvassMessages']):
             canvassMessage = {}
-            canvassMessage['sentimentLabel'] = comment['sentimentLabel']
+            canvassMessage['sentimentLabel'] = comment['meta']['sentimentLabel']
             canvassMessage['details'] = comment['details']['userText']
             canvassMessage['reactionStats'] = comment['reactionStats']
             canvassMessages[c_id] = canvassMessage
-            try:
-                save_comments(session, news_dict['title'], news_dict['content'], news_dict['comment_num'],
-                              news_dict['press_name'], news_dict['content_id'], news_dict['time'],
-                              readingUserCount, sentiment_count, canvassMessages)
-            except:
-                traceback.print_exc()
+        save_comments(session, news_dict['news_url'], news_dict['title'], news_dict['content'], data['total']['count'],
+                      news_dict['press_name'], news_dict['content_id'],
+                      readingUserCount, sentiment_count, canvassMessages)
     except:
         traceback.print_exc()
 
 
-def save_comments(session, title, content, comment_num, press_name, content_id, time, reading_user_count,
+def save_comments(session, news_url, title, content, comment_num, press_name, content_id, reading_user_count,
                   sentiment_count, canvass_messages):
     cur = session.cursor()
-    sql = 'insert into news_comment(title, content, comment_num, press_name, content_id, time,' \
+    sql = 'insert into news_comment(news_url, title, content, comment_num, press_name, content_id,' \
           ' reading_user_count, sentiment_count, canvass_messages) values(%s, %s, %s, %s, %s, %s, %s, %s, %s)'
     t_list = (
-    title, content, comment_num, press_name, content_id, time, reading_user_count, sentiment_count, canvass_messages)
+        news_url, title, content, str(comment_num), press_name, str(content_id), str(reading_user_count), str(sentiment_count),
+        str(canvass_messages))
+    print sql
+    print t_list
     cur.execute(sql, t_list)
     session.commit()
 
@@ -153,7 +168,12 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='1234', db='yahoo_news', charset='utf8')
     yahoo_mainpage_head, yahoo_mainpage_content = get_news_mainpage()
     url_list = parse_news_from_mainpage(yahoo_mainpage_content)
-    for news_url in url_list:
+    acc_num = 5
+    for acc_id, news_url in enumerate(url_list):
+        if acc_id < acc_num:
+            continue
         news_dict = parse_news_title_and_content(news_url)
+        if news_dict is None:
+            continue
         rurl = urlencode(news_dict['content_id'], news_dict['comment_num'])
         parse_comments(conn, rurl, news_dict)
